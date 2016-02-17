dc8b934f321e110eefa2c7c427bc64c0c48a3570
mrockway <mrockway@gmail.com>
2016-02-17 13:25:20 -0800

    remove custom js file

--- main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-
-	// // Show and hide content from menu bar
-	// $('.presentation').click( function(event) {
-	// 	$('.presentation').removeClass('active');
-	// 	$(this).addClass('active');
-	// });
-});
